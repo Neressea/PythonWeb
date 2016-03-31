98f4746588935a4dc375f717e49a212ed71e0e9a
albert22u <vincent.albert@telecomnancy.net>
2016-03-31 13:06:21 +0200

    Push

--- BDD python/main.py
+++ BDD python/main.py
@@ -27,7 +27,7 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
     	self.response.write(*a, **kw)
 
-    def renderstr(self, template, **params):
+    def render_str(self, template, **params):
     	t = jinja_env.get_template(template)
     	return t.render(params)
 
@@ -44,7 +44,8 @@ class MainPage(Handler):
 
 		if title and art:
 			self.write("thanks!")
-		else
+		else:
 			error = "we need both title and art"
+			self.render("front.html", error = error)
 
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
